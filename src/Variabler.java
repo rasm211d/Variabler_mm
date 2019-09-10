2cf8076d31cf3887619253e48e81782e13bdb7fb
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2019-09-10 14:49:55 +0200

    Initial commit

---
+++ src/Variabler.java
@@ -0,0 +1,29 @@
+public class Variabler {
+    public static void main(String[] args) {
+
+        // Hele tal / primitive datatyper
+        byte a = 123;
+        short b = 30000;
+        int c = 212345;
+        long d = 100041451;
+
+        // Decimal tal
+        double e = 1.2;
+        float f = 2.22f;
+
+        //Boolean
+        boolean g = false;
+        boolean h = true;
+
+        // String
+        String i = "Pølse";
+
+        // Character
+        char j = 'k';
+
+        System.out.println();
+
+
+
+    }
+}
